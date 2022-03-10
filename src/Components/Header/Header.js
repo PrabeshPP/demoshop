0e2c6f00da8757a5bc4accd19853422893981ffd
Prabesh <prabesh.bista369@gmail.com>
2022-03-10 10:03:52 +0545

    Header Name completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import classes from "./Header.module.css";
+import HeaderName from "./Header_Name";
 
 const Header=()=>{
     return(
         <div className={classes.header}>
+        <HeaderName/>
 
         </div>
     )
---
+++ src/Components/Header/Header_Name.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Header_Name.module.css";
+
+function HeaderName(){
+    return(
+        <div className={classes["header-Name"]}>
+            <span>Demo Shop</span>
+        </div>
+
+    )
+}
+
+export default HeaderName;
