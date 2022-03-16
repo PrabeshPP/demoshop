555fa8fc14f5d374c1efb92d2b5af7013a4a718a
Prabesh <prabesh.bista369@gmail.com>
2022-03-16 10:21:50 +0545

    added cart

---
+++ src/Components/UI/Modal.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BackDrop=(props)=>{
+    return <div className={classes} ></div>
+
+}
+
+const Modal=(props)=>{
+    return <React.Fragment>
+        <BackDrop/>
+    </React.Fragment>
+}
+
+export default Modal;
